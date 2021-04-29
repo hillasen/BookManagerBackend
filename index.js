5c7ce8cfc4613761b743ec035ec2b2e2fef322e1
hillasen <admin@hillasen.com>
2021-04-29 21:14:05 +0900

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 var mysql = require('mysql');
 const security = require('./security.js');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 const dbSecurity = security.dbSecurity;
 const connection = mysql.createConnection({
     host : dbSecurity.host,
